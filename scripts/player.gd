a07f74f109811bf7b49a86107960ea7f01eba860
cookiemonsternz <Christopher.body@mcglashan.school.nz>
2024-01-15 18:52:03 +1300

    Initial commit

---
+++ scripts/player.gd
@@ -0,0 +1,39 @@
+extends CharacterBody3D
+
+signal close_window
+
+
+@export var speed = 5.0
+@export var jump_velocity = 4.5
+@export var mouse_sens = 0.4
+# Get the gravity from the project settings to be synced with RigidBody nodes.
+var gravity = ProjectSettings.get_setting("physics/3d/default_gravity")
+
+func _ready():
+	Input.mouse_mode = Input.MOUSE_MODE_CAPTURED
+
+func _physics_process(delta):
+	# Add the gravity.
+	if not is_on_floor():
+		velocity.y -= gravity * delta
+
+	# Handle Jump.
+	if Input.is_action_just_pressed("jump") and is_on_floor():
+		velocity.y = jump_velocity
+
+	# Get the input direction and handle the movement/deceleration.
+	# As good practice, you should replace UI actions with custom gameplay actions.
+	var input_dir = Input.get_vector("move_left", "move_right", "move_forward", "move_back")
+	var direction = (transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
+	if direction:
+		velocity.x = direction.x * speed
+		velocity.z = direction.z * speed
+	else:
+		velocity.x = move_toward(velocity.x, 0, speed)
+		velocity.z = move_toward(velocity.z, 0, speed)
+
+	move_and_slide()
+
+func _unhandled_input(event):
+	if(event.is_action("close_window")):
+		close_window.emit()
